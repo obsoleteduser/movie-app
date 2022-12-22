d5105fade571ad11d85cc31df20c7617ef32983b
Tahir Dibirov II <dibrovtahir@gmail.com>
2022-12-22 11:35:27 +0400

    Last Step

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,29 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import './card.css'
 
 export const Card = ({img, title, year, favorites, setFavorites, data})=>{
+    const [active, setActive] = useState(true)
 
     const titleRef = useRef()
 
-    const addToFavs = ()=>{
+    const addToFavs = (event)=>{
         console.log(titleRef.current.textContent)
         let selected = data.filter(item=>item.Title === titleRef.current.textContent)[0]
         selected.favorite = true
         setFavorites([...favorites, selected])
-        console.log(favorites)
+        event.target.setAttribute('disabled', true)
+        setActive(false)
+    }
+
+    const removeFromfavs = ()=>{
+        console.log(data)
+       
     }
 
     return(
         <div className="card borderBlink">
             
-            <i  onClick={addToFavs} className="fa-regular fa-heart heart"></i>
+            <i  onClick={active ? addToFavs : removeFromfavs} className="fa-regular fa-heart heart"></i>
             
             <img src={img} alt="" />
             <div className="info">
